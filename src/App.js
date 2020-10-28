1b30b1f419979ab5a5795aeb1587b29a2b6a4553
MaksimLialkin <maksimlialkin1986@gmail.com>
2020-10-28 16:32:43 +0300

    start first project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='wrapper'>
+      <header className='header'>
+       <img src='https://www.designmantic.com/blog/wp-content/uploads/2019/11/Travel-Logo-1-1.jpg' />
       </header>
+      <nav className='menu'>
+          <ul>
+            <li>Profile</li>
+            <li>Messages</li>
+            <li>News</li>
+            <li>Music</li>
+            <li>Settings</li>
+          </ul>
+        </nav>
+      <div className='content'>
+        <div>
+          <img src='https://images.pexels.com/photos/753626/pexels-photo-753626.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' />
+        </div>
+        <div>
+          <img src='https://images.pexels.com/photos/1049764/pexels-photo-1049764.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' />
+          description
+        </div>
+        <div>My posts</div>
+        <div>New posts</div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
     </div>
   );
 }
+
 
 export default App;
